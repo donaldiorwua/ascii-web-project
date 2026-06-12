5f680839e946e496a65dd64451c084203be7ee1c
donaldiorwua <donaldiorwua@gmail.com>
2026-06-12 20:46:59 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ascii-web-project
+
+go 1.22.2
--- main.go
+++ main.go
@@ -1,2 +1,15 @@
 package main
 
+import (
+	asciiwebproject "ascii-web-project/functions"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", asciiwebproject.HelloApp)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("Not found", err)
+	}
+}
