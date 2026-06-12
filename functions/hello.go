3d5d8bcdc2cbe0a448e320932f2df328486e388e
donaldiorwua <donaldiorwua@gmail.com>
2026-06-12 21:14:25 +0100

    Printed request form to standard output

--- functions/hello.go
+++ functions/hello.go
@@ -3,10 +3,19 @@ package asciiwebproject
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
+// function to render Hello World
 func HelloApp(web http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
 	fmt.Println("server running...")
+
+	// range over the request form and print its content to the terminal
+	for key, value := range request.Form{
+		fmt.Println("Key: ", key)
+		fmt.Println("Value: ", strings.Join(value, " "))
+	}
+	
 	fmt.Fprintf(web, "Hello World")
 }
